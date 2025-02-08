e5a3430919c64f345487be8f69da97560cdc72b8
Tarun Patil <tarunpatil001@gmail.com>
2025-02-08 16:45:44 +0530

    client: Product image in inner zoom properly fixed

--- client/src/components/ProductItem/ImageChanger/ProductImageOpacity.jsx
+++ client/src/components/ProductItem/ImageChanger/ProductImageOpacity.jsx
@@ -5,12 +5,12 @@ const ProductImageOpacityChange = (props) => {
         <div className="h-full w-full overflow-hidden relative">
             <img
                 src={props?.firstImg}
-                className="w-full"
+                className="w-full h-full"
             />
             <img
                 src={props?.SecondImg}
                 alt="product image"
-                className="w-full absolute top-0 left-0 group-hover:scale-110 opacity-0 group-hover:opacity-100 transition-all duration-700 ease-in-out"
+                className="w-full h-full absolute top-0 left-0 group-hover:scale-110 opacity-0 group-hover:opacity-100 transition-all duration-700 ease-in-out"
             />
         </div>
     )
--- client/src/components/ProductZoom/index.jsx
+++ client/src/components/ProductZoom/index.jsx
@@ -4,7 +4,7 @@ import 'react-inner-image-zoom/lib/InnerImageZoom/styles.min.css';
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
-import { Navigation } from "swiper/modules";
+import { Autoplay, Navigation } from "swiper/modules";
 import { useRef, useState } from 'react';
 // import { Link } from "react-router-dom";
 
@@ -33,63 +33,66 @@ const ProductZoom = (props) => {
 
     return (
         <>
-            <div className="flex gap-3 sticky top-20 z-[99] stickyZoom">
-                <div className="slider w-[15%] productDetailImageOptions">
+            <div className="flex gap-3 sticky top-20 z-[99] stickyZoom h-[500px] w-auto">
+                <div className="slider w-[10%] productDetailImageOptions flex flex-col items-center justify-center relative py-9">
                     <Swiper
                         ref={zoomSlideSml}
-                        slidesPerView={5}
-                        centeredSlides={true} // Ensures the active slide is centered
-                        centeredSlidesBounds={true}
+                        centeredSlides={"true"}
+                        centeredSlidesBounds={"true"}
+                        slidesPerView={"auto"}
                         spaceBetween={10}
                         direction={"vertical"}
-                        navigation={false}
-                        modules={[Navigation]}
-                        onSlideChange={onSlideChange}
-                        className="zoomProductSliderThumbs h-[600px] overflow-hidden"
+                        navigation={{ nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" }} // 👈 Define navigation elements
+                        modules={[Navigation, Autoplay]}
+                        className="zoomProductSliderThumbs absolute h-full overflow-hidden"
                     >
-
-                        {
-                            props?.images?.map((image, index) => (
-                                <SwiperSlide key={index}>
-                                    <div className={`item rounded-md overflow-hidden cursor-pointer group ${slideIndex === index ? "opacity-1" : "opacity-30"}`} onClick={() => goto(index)} onMouseEnter={() => goto(index)}>
-                                        <img src={image} alt="img" className="w-full transition-all group-hover:scale-105" />
-                                    </div>
-                                </SwiperSlide>
-
-                            ))
-                        }
-
-
+                        {props?.images?.map((image, index) => (
+                            <SwiperSlide key={index} className='!w-[40px] !h-[40px]'>
+                                <div
+                                    className={`item !w-[40px] !h-[40px] p-0.5 border border-black rounded-md overflow-hidden cursor-pointer group ${slideIndex === index ? "opacity-1 border-2 !border-blue-600" : "opacity-30"}`}
+                                    onClick={() => goto(index)} onMouseEnter={() => goto(index)}
+                                >
+                                    <img src={image} alt="img" className="w-full h-full object-cover transition-all rounded-md" />
+                                </div>
+                            </SwiperSlide>
+                        ))}
                     </Swiper>
+
+                    {/* Swiper Navigation Buttons (Must be outside Swiper but inside the container) */}
+                    <div className="swiper-button-prev"></div>
+                    <div className="swiper-button-next"></div>
                 </div>
 
-                <div className="zoomContainer w-full h-[600px] max-w-[85%] mx-auto relative">
-                    {/* <div className="w-full h-full border-red-400"> */}
-                    <Swiper
-                        ref={zoomSlideBig}
-                        slidesPerView={1}
-                        spaceBetween={0}
-                        onSlideChange={onSlideChange}
-                        className="productZoomSwiper w-full h-full rounded-md"
-                    >
 
-                        {
-                            props?.images?.map((image, index) => (
-                                <SwiperSlide key={index}>
-                                    <InnerImageZoom
-                                        zoomType="hover"
-                                        zoomPreload={true}
-                                        hideCloseButton={true}
-                                        fullscreenOnMobile={false}
-                                        zoomScale={1}
-                                        src={image}
-                                        className="w-full h-full object-cover" // Ensure image covers the container while maintaining aspect ratio
-                                    />
-                                </SwiperSlide>
+                <div className="zoomContainer w-full h-full max-w-[90%] mx-auto relative shadow rounded-md">
+                    <div className="w-full h-full border-red-400">
+                        <Swiper
+                            ref={zoomSlideBig}
+                            slidesPerView={1}
+                            spaceBetween={0}
+                            onSlideChange={onSlideChange}
+                            className="productZoomSwiper w-full h-full rounded-md"
+                        >
 
-                            ))}
 
-                    </Swiper>
+                            {
+                                props?.images?.map((image, index) => (
+                                    <SwiperSlide key={index}>
+                                        <InnerImageZoom
+                                            zoomType="hover"
+                                            zoomPreload={true}
+                                            hideCloseButton={true}
+                                            fullscreenOnMobile={false}
+                                            zoomScale={1}
+                                            src={image}
+                                            className="w-full h-full object-cover" // Ensure image covers the container while maintaining aspect ratio
+                                        />
+                                    </SwiperSlide>
+
+                                ))}
+
+                        </Swiper>
+                    </div>
                 </div>
             </div>
         </>
